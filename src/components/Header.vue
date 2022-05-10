4b38c53cb38f6b4c50a6f7a9a73ed6e9b7dee507
duongta <duongtac22@gmail.com>
2022-05-11 06:23:28 +0700

    zzz

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="w-full flex items-center gap-4 rounded-lg border-2 border-white p-4">
-      <div class="item">
-          
+  <div class="w-full flex items-center justify-between gap-4 rounded-lg border-2 border-white p-4">
+      <div class="item text-white">
+          <span class="text-[14px]">
+            Status :
+          </span>
+          <span class="text-[20px] font-bold">
+            ONLINE
+          </span>
       </div>
-      <div class="item">
-          
+      <div class="item text-white">
+           <span class="text-[14px]">
+            Limiter :
+          </span>
+          <span class="text-[20px] font-bold">
+            OEL
+          </span>
+      </div>
+      <div class="item text-white">
+           <span class="text-[14px]">
+            PSS :
+          </span>
+          <span class="text-[20px] font-bold">
+            Active
+          </span>
       </div>
       <div class="item">
           <span class="icon w-[48px] h-[48px] inline-block">
