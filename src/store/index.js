234a8e6b03610b2eee035b13991ba544ac33789a
duongta <duongtruonganh@vccorp.vn>
2022-06-30 16:10:47 +0700

    zz

--- src/store/index.js
+++ src/store/index.js
@@ -4,21 +4,39 @@ export const store = createStore({
   state() {
     return {
       user: null,
+      isLogin: false,
+      token: null
     };
   },
   getters: {
-    user: (state) => {
+    getUser: (state) => {
       return state.user;
     },
+    getIsLogin: (state) => {
+      return state.isLogin
+    }
   },
   actions: {
-    user({ commit }, user) {
+    setUser({ commit }, user) {
       commit("user", user);
+    },
+    setToken({ commit }, token) {
+      commit("token", token);
     },
   },
   mutations: {
+    changeLoginStatus: (state, value) => {
+      state.isLogin = value
+    },
     user(state, user) {
       state.user = user;
+    },
+    token(state, token) {
+      state.token = token;
+    },
+    clearState: (state) => {
+      state.isLogin = false
+      state.token = null
     },
   },
 });
